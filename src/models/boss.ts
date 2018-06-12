3580af8d7a11c49e012d62a810f1b1415a7069ac
Matt Rossetta <archanglmr@yahoo.com>
2018-06-11 23:47:39 -0400

    boss has id

--- src/models/boss.ts
+++ src/models/boss.ts
@@ -1,23 +1,25 @@
 import { IBoss } from '../interfaces/boss';
 
 export class Boss implements IBoss {
-    level: number;
-    name: string;
-    cost: number;
-    payTo: string;
-    goal: number;
-    rules: string;
-    prize: string;
-    points: number;
-    source: string;
+  level: number;
+  name: string;
+  cost: number;
+  payTo: string;
+  goal: number;
+  rules: string;
+  prize: string;
+  points: number;
+  source: string;
 
-    active: boolean = false;
+  active: boolean = false;
+  id: string = null;
 
-    constructor(bossData: IBoss) {
-        for (let key in bossData) {
-            if (bossData.hasOwnProperty(key)) {
-                this[key] = bossData[key];
-            }
-        }
+  constructor(bossData: IBoss) {
+    for (let key in bossData) {
+      if (bossData.hasOwnProperty(key)) {
+        this[key] = bossData[key];
+      }
     }
+    this.id = this.name.toLowerCase().replace(/\./g, '').replace(/ /g, '-');
+  }
 }
