9bd27d04b8197950c5146f1db57d9445f17f6c2f
Matt Rossetta <archanglmr@yahoo.com>
2018-06-11 22:48:42 -0400

    spacing

--- src/models/game-character.ts
+++ src/models/game-character.ts
@@ -1,21 +1,21 @@
 import { IGameCharacter } from '../interfaces/game-character';
 
 export class GameCharacter implements IGameCharacter {
-    name: string;
-    star_ability: string;
-    power_up_scope: string;
-    power_up_ability: string;
-    source: string;
+  name: string;
+  star_ability: string;
+  power_up_scope: string;
+  power_up_ability: string;
+  source: string;
 
-    active: boolean = false;
-    id: string = null;
+  active: boolean = false;
+  id: string = null;
 
-    constructor(gameCharacterData: IGameCharacter) {
-        for (let key in gameCharacterData) {
-            if (gameCharacterData.hasOwnProperty(key)) {
-                this[key] = gameCharacterData[key];
-            }
-        }
-        this.id = this.name.toLowerCase().replace(/ /g, '-');
+  constructor(gameCharacterData: IGameCharacter) {
+    for (let key in gameCharacterData) {
+      if (gameCharacterData.hasOwnProperty(key)) {
+        this[key] = gameCharacterData[key];
+      }
     }
+    this.id = this.name.toLowerCase().replace(/ /g, '-');
+  }
 }
--- src/models/property.ts
+++ src/models/property.ts
@@ -1,18 +1,18 @@
 import { IProperty } from '../interfaces/property';
 
 export class Property implements IProperty {
-    color: string;
-    name: string;
-    cost: number;
-    rent: number;
-    monopoly_rent: number;
-    points: number;
+  color: string;
+  name: string;
+  cost: number;
+  rent: number;
+  monopoly_rent: number;
+  points: number;
 
-    constructor(propertyData: IProperty) {
-        for (let key in propertyData) {
-            if (propertyData.hasOwnProperty(key)) {
-                this[key] = propertyData[key];
-            }
-        }
+  constructor(propertyData: IProperty) {
+    for (let key in propertyData) {
+      if (propertyData.hasOwnProperty(key)) {
+        this[key] = propertyData[key];
+      }
     }
+  }
 }
